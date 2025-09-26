68377cd41f4cff76059ca22906b74cb4fbc293b4
leogoes3 <49701370+leogoes3@users.noreply.github.com>
2025-09-26 09:21:20 -0300

    Add files via upload

---
+++ src/test/java/com/example/Agent_Backend/AgentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Agent_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
